04d3b34a65801a81112c7a26c12df082f08adf4e
Marco Conte <mconte@asic-domain.local>
2017-01-18 17:04:27 -0500

    LangtonAntEngine project added.

---
+++ LangtonAntEngine/LangtonAntEngine.h
@@ -0,0 +1,13 @@
+// LangtonAntEngine.h
+
+#pragma once
+
+using namespace System;
+
+namespace LangtonAntEngine {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
