9352c4e16c96e96ec0470d03a2844cf691ed0b6b
Marco Conte <mconte@asic-domain.local>
2017-01-18 17:27:23 -0500

    Added Graphic class to the project.

---
+++ LangtonAntEngine/Graphic.h
@@ -0,0 +1,46 @@
+#pragma once
+
+
+#include <d2d1_2.h>
+
+
+
+namespace lant
+{
+	/// <summary>
+	/// Base graphic element.
+	/// </summary>
+	class Graphic
+	{
+	public:
+
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		Graphic() noexcept
+			: transform(D2D1::IdentityMatrix())
+		{
+		}
+
+
+		/// <summary>
+		/// Draws the graphic element.
+		/// </summary>
+		/// <param name="pRenderTarget">The render target.</param>
+		/// <param name="worldTransform">The viewport matrix transformation.</param>
+		/// <returns></returns>
+		virtual void draw(ID2D1RenderTarget* pRenderTarget, const D2D1_MATRIX_3X2_F& worldTransform) const noexcept = 0;
+
+
+
+	protected:
+
+
+		/// <summary>
+		/// Matrix transformation of the graphic element.
+		/// </summary>
+		D2D1_MATRIX_3X2_F transform;
+
+	};
+}
