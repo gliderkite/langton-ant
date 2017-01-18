9416aca2bd962123f594f9e5a3ceccec986136d5
Marco Conte <mconte@asic-domain.local>
2017-01-18 16:59:35 -0500

    First commit.

---
+++ Langton Ant/PInvoke.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PhaseZero
+{
+    /// <summary>
+    /// P/Invoke functions.
+    /// </summary>
+    internal static class PInvoke
+    {
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
+        /// </summary>
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(
+            int dwExStyle,
+            string lpszClassName,
+            string lpszWindowName,
+            int style,
+            int x,
+            int y,
+            int width,
+            int height,
+            IntPtr hwndParent,
+            IntPtr hMenu,
+            IntPtr hInst,
+            [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms632682(v=vs.85).aspx
+        /// </summary>
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633576(v=vs.85).aspx
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        internal struct WNDCLASS
+        {
+            public uint style;
+            public IntPtr lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string lpszMenuName;
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string lpszClassName;
+        }
+
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633586(v=vs.85).aspx
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern ushort RegisterClassW([In] ref WNDCLASS lpWndClass);
+
+
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633572(v=vs.85).aspx
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr DefWindowProcW(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+        /// <summary>
+        /// https://msdn.microsoft.com/it-it/library/windows/desktop/ms648391(v=vs.85).aspx
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern IntPtr LoadCursor(IntPtr hInstance, int lpCursorName);
+    }
+}
---
+++ Langton Ant/Viewport.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace PhaseZero
+{
+    /// <summary>
+    /// Build Window Core delegate,
+    /// </summary>
+    /// <param name="hwndHost">Viewport host window.</param>
+    public delegate void BuildWindowCoreDelegate(IntPtr hwndHost);
+
+    /// <summary>
+    /// Window main procedure delegate.
+    /// </summary>
+    /// <param name="hWnd">A handle to the window.</param>
+    /// <param name="msg">The message.</param>
+    /// <param name="wParam">Additional message information.</param>
+    /// <param name="lParam">Additional message information.</param>
+    /// <returns></returns>
+    internal delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+    /// <summary>
+    /// Represents the viewport.
+    /// A custom control hosted by a WPF application which rendering can be
+    /// redirect to a native DirectX application.
+    /// </summary>
+    sealed class Viewport : HwndHost
+    {
+
+
+        /// <summary>
+        /// Raised only when the hosted window has been created properly.
+        /// </summary>
+        public event BuildWindowCoreDelegate BuildWindowCoreCompleted;
+
+        /// <summary>
+        /// Processes Windows messages.
+        /// </summary>
+        public static event WndProcDelegate ProcessMessage;
+
+        /// <summary>
+        /// Window procedure delegate.
+        /// </summary>
+        private WndProcDelegate wproc;
+
+        /// <summary>
+        /// Hosted window.
+        /// </summary>
+        private IntPtr hwndHost;
+        
+
+
+        /// <summary>
+        /// Create the window to host.
+        /// </summary>
+        /// <param name="hwndParent">Parent window handle.</param>
+        /// <returns></returns>
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            // Register the window class.
+            var wcex = new PInvoke.WNDCLASS();
+
+            const uint CS_HREDRAW = 0x0002;
+            const uint CS_VREDRAW = 0x0001;
+            const int WS_CHILD = 0x40000000;
+            const int WS_VISIBLE = 0x10000000;
+            const int IDC_ARROW = 32512;
+            const string className = "PhaseZeroViewportClass";
+
+            wcex.style = CS_HREDRAW | CS_VREDRAW;
+            wcex.lpszClassName = className;
+            wcex.hCursor = PInvoke.LoadCursor(IntPtr.Zero, IDC_ARROW);
+            wcex.lpfnWndProc = Marshal.GetFunctionPointerForDelegate(wproc = CustomWndProc);
+
+            // register class and check failure
+            if (PInvoke.RegisterClassW(ref wcex) == 0)
+                return new HandleRef(this, IntPtr.Zero);
+            
+            // create the viewport host window
+            hwndHost = PInvoke.CreateWindowEx(
+                0,                      // extended style
+                className,              // class name
+                String.Empty,           // window name
+                WS_CHILD | WS_VISIBLE,  // style
+                0,                      // x
+                0,                      // y
+                0,                      // width
+                0,                      // height
+                hwndParent.Handle,      // parent window handle
+                IntPtr.Zero,            // menu
+                IntPtr.Zero,            // instance
+                0);                     // params
+
+            // invoke the event to signal the host window has been properly created
+            if (hwndHost != IntPtr.Zero)
+                BuildWindowCoreCompleted?.Invoke(hwndHost);
+
+            return new HandleRef(this, hwndHost);
+        }
+
+
+        /// <summary>
+        /// Destroy the hosted window.
+        /// </summary>
+        /// <param name="hwnd">Hosted window.</param>
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            PInvoke.DestroyWindow(hwnd.Handle);
+        }
+
+
+        /// <summary>
+        /// Custom window procedure.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window.</param>
+        /// <param name="msg">The message.</param>
+        /// <param name="wParam">Additional message information.</param>
+        /// <param name="lParam">Additional message information.</param>
+        /// <returns></returns>
+        private static IntPtr CustomWndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            var ptr = ProcessMessage?.Invoke(hWnd, msg, wParam, lParam);
+
+            if (!ptr.HasValue)
+                ptr = PInvoke.DefWindowProcW(hWnd, msg, wParam, lParam);
+
+            Debug.Assert(ptr.HasValue);
+            return ptr.Value;
+        }
+        
+        
+    }
+}
